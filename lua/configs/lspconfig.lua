0aa3e8d9ed5bfabd121d86f954b88d1feee272af
Anton Bastin <to4kin@gmail.com>
2025-10-01 10:26:09 +0200

    feat: Migrate to the new API

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,22 +1,24 @@
--- EXAMPLE
+-- Using new vim.lsp.config API (Neovim 0.11+)
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "ts_ls", "gopls", "bashls", "dockerls", "terraformls" }
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+-- Default config for most servers
+local default_config = {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+-- Setup LSP servers with default config
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, default_config)
 end
 
--- helm-ls
-lspconfig.helm_ls.setup {
+-- helm-ls with custom settings
+vim.lsp.config('helm_ls', vim.tbl_extend('force', default_config, {
   settings = {
     ['helm-ls'] = {
       yamlls = {
@@ -24,7 +26,7 @@ lspconfig.helm_ls.setup {
       }
     }
   }
-}
+}))
 
--- setup yamlls
-lspconfig.yamlls.setup {}
+-- yamlls
+vim.lsp.config('yamlls', default_config)
