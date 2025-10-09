74c566e69028062259a8ed90ede32cc06d94120d
Anton Bastin <to4kin@gmail.com>
2025-10-09 09:51:41 +0200

    Update dependencies and fix nvchad

---
+++ lua/nvchad/cmp.lua
@@ -0,0 +1,5 @@
+-- This module provides custom completion (cmp) overrides
+-- It's required by NvChad's cmp.lua configuration but was missing from the default setup
+-- You can add your custom cmp settings here to override NvChad defaults
+
+return {}
